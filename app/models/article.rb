f72162662c9ef4781956d1753d4ae6e755f35445
itsurboymk37 <markush@nuevaschool.org>
2020-08-19 12:56:58 -0700

    going back to my school laptop! partway through tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+    has_many :taggings
+    has_many :tags, through: :taggings
+
+    def tag_list
+        self.tags.collect do |tag|
+          tag.name
+        end.join(", ")
+    end        
 end
